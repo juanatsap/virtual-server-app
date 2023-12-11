cbff80cc882353d5ae929dc069fe6d052cd534de
juanatsap <juanatsap@gmail.com>
2023-12-11 19:52:34 +0100

    added https and working on EDGE but not on CHROME

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const https = require('https');
+const fs = require('fs');
 const app = express();
 const port = 3000;
 
+const privateKey = fs.readFileSync('https_certs/localhost-key.pem', 'utf8');
+const certificate = fs.readFileSync('https_certs/localhost.pem', 'utf8');
+const credentials = { key: privateKey, cert: certificate };
+
 app.use(express.static('public'));
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+const httpsServer = https.createServer(credentials, app);
+
+httpsServer.listen(port, () => {
+  console.log(`Server running at https://localhost:${port}`);
 });
